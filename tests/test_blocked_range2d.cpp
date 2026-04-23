4d914a7a1b6f9fbf8947b2d34283c901569de557
Sebastian <sebastian.lotman@gmail.com>
2026-04-23 10:42:19 +0300

    minimal testing

---
+++ tests/test_blocked_range2d.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include "oneapi/tbb/blocked_range2d.h"
+
+int main() {
+    tbb::blocked_range2d<int> r(0, 10, 2, 0, 20, 4);
+
+    assert(!r.empty());
+    assert(r.is_divisible());
+
+    tbb::blocked_range2d<int> r2(r, tbb::split{});
+
+    assert(r.rows().end() == r2.rows().begin() ||
+           r.cols().end() == r2.cols().begin());
+
+    return 0;
+}
